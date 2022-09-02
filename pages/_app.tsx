69b9aa0dfc8f37d9f0264c31f19ab4fa8c0133df
Aziz Abbas <aziz@builder.io>
2022-09-02 09:49:47 -0700

    try explicit require

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
 import type { AppProps } from 'next/app'
 
-import { builder } from '@builder.io/react'
+import { builder, Builder } from '@builder.io/react'
 import builderConfig from '@config/builder'
+
+if (Builder.isServer) {
+  try {
+    // tricking whatever stripping vm2 to include it on server
+    require('vm2');
+  } catch(e) {
+    console.error(e)
+  }
+}
+
 builder.init(builderConfig.apiKey)
 
 export default function MyApp({ Component, pageProps }: AppProps) {
